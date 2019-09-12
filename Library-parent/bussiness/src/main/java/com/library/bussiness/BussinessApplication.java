4ad6d6c3e03f0c6911b3889c48a1e6bbc3a6a404
dattran01477 <dattran01477@gmail.com>
2019-09-12 07:35:33 +0700

    move child project to parent project

---
+++ Library-parent/bussiness/src/main/java/com/library/bussiness/BussinessApplication.java
@@ -0,0 +1,20 @@
+package com.library.bussiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+import com.library.dao.DaoApplication;
+
+@SpringBootApplication
+@PropertySources({ @PropertySource("application-bussiness.properties") })
+public class BussinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BussinessApplication.class, args);
+	}
+
+}
