ec2e18ae98aabaec05883a3923892664a8b3765c
dattran01477 <dattran01477@gmail.com>
2019-10-02 07:43:51 +0700

    add security keycloak

--- web/src/main/java/com/library/web/WebApplication.java
+++ web/src/main/java/com/library/web/WebApplication.java
@@ -2,11 +2,13 @@ package com.library.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class })
 @ComponentScan(basePackages = "com.library")
 @PropertySources({ @PropertySource("application-web.properties") })
 public class WebApplication {
